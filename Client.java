7088caa01a9be0f886662987bfba4b6586af452c
razalberto <raz_alberto@yahoo.com>
2021-12-01 22:27:36 +0200

    Verifying user's status before sending the message

--- Client.java
+++ Client.java
@@ -1,13 +1,19 @@
+
+
 import com.rabbitmq.client.*;
 
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeoutException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Client implements AutoCloseable{
@@ -17,14 +23,13 @@ public class Client implements AutoCloseable{
     private Connection connection;
     private Channel channel;
     private String username ;
-
-
+    Timer timer = new Timer();
 
     private static final String EXCHANGE_ROOM = "generalTopic";
     private static final String EXCHANGE_PRIVATE = "directTopic";
-    private static final String EXCHANGE_SERVER = "serverTopic";
-    private static final String SERVER_QUEUE = "serverQueue";
-    private static final String ONLINE_CHECK = "onlineQueue";
+    private static final String SERVER_QUEUE = "serverQueue"; //used to connect to server; verify name uniqueness
+    private static final String ONLINE_CHECK = "onlineQueue"; //used to send pings once every 500 ms
+    private static final String PRIVATE_CHECK_ONLINE = "checkOnlineStatus";
 
 
 
@@ -37,7 +42,6 @@ public class Client implements AutoCloseable{
 
         channel.exchangeDeclare(EXCHANGE_ROOM, "topic");
         channel.exchangeDeclare(EXCHANGE_PRIVATE, "direct");
-        channel.exchangeDeclare(EXCHANGE_SERVER, "topic");
     }
 
 
@@ -45,16 +49,56 @@ public class Client implements AutoCloseable{
 
     public static void main(String[] args) throws Exception {
         try(Client client = new Client()){
+            Scanner s = new Scanner(System.in);
             client.username = client.decideUsername();
-            System.out.println("My name is "+ client.username);
 
-            while(true){
-                long timeAtLoopStart = System.currentTimeMillis();
-                long currentTime = System.currentTimeMillis();
-                while(currentTime-timeAtLoopStart < 500){
-                    currentTime = System.currentTimeMillis();
+            System.out.println("Hello, "+client.username+". Welcome to the chat room!");
+            helpDisplay();
+
+            client.timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run(){
+                    try{
+                        client.sendOnlineStatus(client.username);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
                 }
-                client.sendOnlineStatus(client.username);
+            },0, 500);
+
+            //CallBack when a message is consumed;
+	        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+		        String message = new String(delivery.getBody(), "UTF-8");
+		        System.out.println(message);
+	    	};
+
+	    	//random queue name :
+        	String queueName = client.channel.queueDeclare().getQueue(); 
+
+        	//Binding queue to our exchanges:
+	        client.channel.queueBind(queueName, EXCHANGE_ROOM, "");
+	        client.channel.queueBind(queueName, EXCHANGE_PRIVATE, client.username);
+
+            client.channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+
+            boolean connected=true;
+            while(connected){
+                String message = s.nextLine();
+                if(message.startsWith("--")){
+                    switch(message.charAt(2)){
+                        case 'p':
+                            String[] params = message.split(" ", 3);
+                            if(params.length < 3)
+                                System.out.println("Usage: --p <Name> <Message>");	                        
+                            else 
+                                client.sendPrivateMessage(params[2], params[1]);
+                            break;
+                    }
+                }else{
+                    System.out.println("Unknown command: \n");
+                    System.out.println("\t: "+message.split(" ")[0]+"\n");
+                    helpDisplay();
+                }
             }
 
         }catch(IOException | TimeoutException | InterruptedException e){
@@ -70,21 +114,14 @@ public class Client implements AutoCloseable{
     private String decideUsername() throws IOException, InterruptedException {
         //Set the username before loop
         Scanner s = new Scanner(System.in);
-        System.out.print("type your name : ");
+        System.out.print("Choose a username: ");
         String username = s.nextLine();
-
-
-
-        //set random queueName and correlationID for RPC before loop
-
-
-
 
         //loop while username is already taken
         Boolean result = false;
 
         while(result == false){
-
+            //set random queueName and correlationID for RPC before loop
             final String corrId = UUID.randomUUID().toString();
             String replyQueueName = channel.queueDeclare().getQueue();
             AMQP.BasicProperties props = new AMQP.BasicProperties
@@ -94,11 +131,7 @@ public class Client implements AutoCloseable{
                     .build();
             channel.basicPublish("", SERVER_QUEUE, props, username.getBytes("UTF-8"));
 
-
-
             final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
-
-
 
             String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
                 if (delivery.getProperties().getCorrelationId().equals(corrId)) {
@@ -110,8 +143,8 @@ public class Client implements AutoCloseable{
 
 
             String feedback = response.take();
-            result = feedback.equals("true");
             channel.basicCancel(ctag);
+            result = feedback.equals("true");
             if(!result) {
                 System.out.print("Username is already taken, type another : ");
                 username = s.nextLine();
@@ -120,9 +153,54 @@ public class Client implements AutoCloseable{
         return username;
     }
 
+    private void sendPrivateMessage(String message, String dest) throws IOException, InterruptedException{
 
+        if(dest.equals(username)){
+            System.out.println("You can't send yourself messages");
+            return;
+        }
+
+		final String corrId = UUID.randomUUID().toString();
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+        channel.basicPublish("", PRIVATE_CHECK_ONLINE, props, dest.getBytes("UTF-8"));
+
+        final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.offer(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String isOnline = response.take();
+        channel.basicCancel(ctag);
+
+        String finalMessage="";
+        if(isOnline.equals("true")){
+            finalMessage ="Private message from " + username + ": " + message;
+            channel.basicPublish(EXCHANGE_PRIVATE, dest, null, finalMessage.getBytes());
+        }else{
+            System.out.println("User "+dest+" is offline. Try another time");
+        }
+
+    }
+
+    private static void helpDisplay(){
+        System.out.println("******************** HELP ********************");
+        System.out.println("*  --b <message>        : Broadcast message  *");
+        System.out.println("*  --p <name> <message> : Private message    *");
+        System.out.println("*  --q                  : Leave the chat     *");
+        System.out.println("**********************************************\n");
+    }
 
     public void close() throws IOException {
+        timer.cancel();
         connection.close();
     }
 
--- Server.java
+++ Server.java
@@ -10,27 +10,27 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
+class User {
+    public String username;
+    public long timestamp;
+
+    User(String username, long timestamp){
+        this.username=username;
+        this.timestamp=timestamp;
+    }
+}
 
 public class Server implements AutoCloseable {
 
-    class User {
-        public String username;
-        public long timestamp;
-
-        User(String username, long timestamp){
-            this.username=username;
-            this.timestamp=timestamp;
-        }
-    }
     private Connection connection;
     private Channel channel;
     private static ArrayList<User> userList = new ArrayList<User>();
     Timer timer = new Timer();
 
-
-
     private static final String SERVER_QUEUE = "serverQueue";
     private static final String ONLINE_CHECK = "onlineQueue";
+    private static final String PRIVATE_CHECK_ONLINE = "checkOnlineStatus";
+
 
 
     public Server() throws IOException, TimeoutException {
@@ -44,6 +44,8 @@ public class Server implements AutoCloseable {
         channel.queuePurge(SERVER_QUEUE);
         channel.queueDeclare(ONLINE_CHECK, false, false, false, null);
         channel.queuePurge(ONLINE_CHECK);
+        channel.queueDeclare(PRIVATE_CHECK_ONLINE, false, false, false, null);
+        channel.queuePurge(PRIVATE_CHECK_ONLINE);
         channel.basicQos(1);
     }
 
@@ -61,25 +63,49 @@ public class Server implements AutoCloseable {
                 }
             },0, 1000);
 
-            Object monitor = new Object();
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+
+            Object monitor1 = new Object();
+            DeliverCallback usernameCallback = (consumerTag, delivery) -> {
                 AMQP.BasicProperties replyProps = new AMQP.BasicProperties
                         .Builder()
                         .correlationId(delivery.getProperties().getCorrelationId())
                         .build();
                 String response = "";
                 try {
-                    String message = new String(delivery.getBody(), "UTF-8");
-                    System.out.println("[VERIFY] "+ message + " existance");
-                    response = server.verifyUsername(message);
+                    String username = new String(delivery.getBody(), "UTF-8");
+                    System.out.println("[VERIFY] "+ username + " existance");
+                    response = server.verifyUser(username);
                 } catch (RuntimeException e) {
                     System.out.println(" [ERROR] " + e.toString());
                 } finally {
                     server.channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
                     server.channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
                     // RabbitMq consumer worker thread notifies the RPC server owner thread
-                    synchronized (monitor) {
-                        monitor.notify();
+                    synchronized (monitor1) {
+                        monitor1.notify();
+                    }
+                }
+            };
+
+            Object monitor2 = new Object();
+            DeliverCallback privateCallback = (consumerTag, delivery) -> {
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                        .Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+                String response = "";
+                try {
+                    String username = new String(delivery.getBody(), "UTF-8");
+                    System.out.println("[PRIVATE] "+ username + " status");
+                    response += server.verifyStatus(username);
+                } catch (RuntimeException e) {
+                    System.out.println(" [ERROR] " + e.toString());
+                } finally {
+                    server.channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                    server.channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                    // RabbitMq consumer worker thread notifies the RPC server owner thread
+                    synchronized (monitor2) {
+                        monitor2.notify();
                     }
                 }
             };
@@ -87,20 +113,30 @@ public class Server implements AutoCloseable {
             DeliverCallback onlineCallback = (consumerTag, delivery) -> {
                 String message = new String(delivery.getBody(), "UTF-8");
                 try {
-                    server.verifyUser(message);
+                    server.updateStatus(message);
                 } finally {
                     server.channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
                 }
             };
 
+            
+
             server.channel.basicConsume(ONLINE_CHECK, false, onlineCallback, consumerTag -> { });
-            server.channel.basicConsume(SERVER_QUEUE, false, deliverCallback, (consumerTag -> { }));
+            server.channel.basicConsume(SERVER_QUEUE, false, usernameCallback, (consumerTag -> { }));
+            server.channel.basicConsume(PRIVATE_CHECK_ONLINE, false, privateCallback, (consumerTag -> { }));
 
             // Wait and be prepared to consume the message from RPC client.
             while (true) {
-                synchronized (monitor) {
+                synchronized (monitor1) {
                     try {
-                        monitor.wait();
+                        monitor1.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                synchronized (monitor2) {
+                    try {
+                        monitor2.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -119,8 +155,15 @@ public class Server implements AutoCloseable {
     }
 
 
-    private String verifyUsername(String username){
-        Boolean verify = userList.contains(username);
+    private String verifyUser(String username){
+	Boolean verify=false;
+	for(User u : userList){
+            if(u.username.equals(username)){
+                verify=true;
+		break;
+            }
+	}
+       
         if(verify){
             return "false";
         }
@@ -128,7 +171,20 @@ public class Server implements AutoCloseable {
         return "true";
     }
 
-    private static void verifyUser(String username) {
+    private String verifyStatus(String username){
+        Boolean verify=false;
+        for(User u: userList){
+            if(u.username.equals(username)){
+                verify=true;
+                break;
+            }
+        }
+        if(verify)
+            return "true";
+        return "false";
+    }
+
+    private static void updateStatus(String username) {
         long currentTime = System.currentTimeMillis();
         for(User u : userList){
             if(u.username.equals(username)){
